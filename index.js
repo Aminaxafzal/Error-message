a315c04d2711d33f6d2a68af9f33d70dd421e223
minamedia <mina.m3dia@gmail.com>
2021-07-16 16:08:25 +0100

    error-message

---
+++ index.js
@@ -0,0 +1,13 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+
+let errorMessage = document.getElementById("error")
+
+
+function alertError() {
+
+errorMessage.textContent +="Something went wrong, please try again"
+
+}
